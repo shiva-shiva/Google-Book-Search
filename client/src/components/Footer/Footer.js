ed92748a10ae70fb6ad33ba9a6885fa12b3e7846
shiva sabokdast <shiva.sabokdast@gmail.com>
2021-03-27 09:42:02 -0400

    add component template

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
         <>  
             <hr />
             <div id="footer-padding"></div>
-            <div id="footer">(c) 202x Project#3 Team</div>
+            <div id="footer"></div>
         </>
     )
 }
---
+++ client/src/components/Navbar/NavBar.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import { Redirect, NavLink, useLocation  } from "react-router-dom"
+import { useStoreContext } from "../../utils/GlobalStore"
+
+let timeout
+
+function NavBar() {
+  
+  return ( 
+    <>
+    </>
+  )
+}
+
+export default NavBar
